5f37aede628752a6354b267b4aded3925f0741fb
David Turner <david.turner@elastic.co>
2018-04-23 17:34:25 +0100

    Highlight unproved conclusions too

--- ZenWithTerms/isabelle/mkInvariants.pl
+++ ZenWithTerms/isabelle/mkInvariants.pl
@@ -3,29 +3,40 @@
 use strict;
 
 my @assms = ();
+my $conclusion;
+my $unproved = 0;
 
 print <<EOF;
 digraph G {
   rankdir="LR"
-  node[color="red",penwidth=4]
+  node[color="red",shape="box",penwidth=4]
 EOF
 
 while (<>) {
   chomp;
   if (/^lemma/) {
+
+    if ($conclusion) {
+      my $conclusionColour = $unproved ? 'goldenrod' : 'forestgreen';
+      print "  $conclusion [color=\"$conclusionColour\"]\n";
+      for my $assm (@assms) {
+        my $assmname = $assm->{assm};
+        next if $assmname eq $conclusion;
+        my $style = $assm->{state} eq 's' ? '[arrowhead="normal",color="forestgreen",penwidth=2]' : '[arrowhead="onormal",color="goldenrod",penwidth=2]';
+        print "  $assmname -> $conclusion $style\n";
+      }
+    }
+
     @assms = ();
+    undef $conclusion;
+    $unproved = 0;
+
   } elsif (/^  assumes "([st]) *\\<Turnstile> *(\w+)/) {
     unshift @assms, {state => $1, assm => $2};
   } elsif (/^  shows.*\\<Turnstile> *(\w+)/) {
-    my $conclusion = $1;
-    print "$conclusion [shape=\"box\",color=\"black\"]\n";
-    for my $assm (@assms) {
-      my $assmname = $assm->{assm};
-      next if $assmname eq $conclusion;
-      my $style = $assm->{state} eq 's' ? '[arrowhead="normal",penwidth=2]' : '[arrowhead="onormal",color="red",penwidth=2]';
-      print "  $assmname -> $conclusion $style\n";
-    }
-    @assms = ();
+    $conclusion = "$1";
+  } elsif (/sorry/) {
+    $unproved = 1;
   }
 }
 
